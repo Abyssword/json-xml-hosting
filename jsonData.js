8eb71788e3c75a49249387f662990218b7f72afa
Eliah Kim Dichoso <62477593+Abyssword@users.noreply.github.com>
2024-10-08 11:59:40 +0800

    Add files via upload

---
+++ jsonData.js
@@ -0,0 +1,26 @@
+const jsonData = {
+    "Catalog": {
+        "Products": [
+            {
+                "ProductID": "101",
+                "Name": "Smartphone",
+                "Category": "Electronics",
+                "Price": 299.99,
+                "Stock": 25,
+                "Description": "A high-end smartphone  by SAMSUNG with 128GB storage.",
+                "ImageURL": "https://images.samsung.com/is/image/samsung/p6pim/ph/feature/others/ph-feature-galaxy-s24-fe-s721-543711492?$900_N_JPG$"
+            },
+            {
+                "ProductID": "102",
+                "Name": "Laptop",
+                "Category": "Electronics",
+                "Price": 799.99,
+                "Stock": 15,
+                "Description": "A powerful laptop for work and gaming.",
+                "ImageURL": "https://www.techtarget.com/rms/onlineimages/hp_elitebook_mobile.jpg"
+            }
+        ]
+    }
+};
+
+module.exports = jsonData;
---
+++ xmlData.js
@@ -0,0 +1,21 @@
+const xmlData = `
+<Catalog>
+    <Product ProductID="101">
+        <Name>Smartphone</Name>
+        <Category>Electronics</Category>
+        <Price>299.99</Price>
+        <Stock>25</Stock>
+        <Description>A high-end smartphone with 128GB storage.</Description>
+        <ImageURL>http://example.com/images/smartphone.jpg</ImageURL>
+    </Product>
+    <Product ProductID="102">
+        <Name>Laptop</Name>
+        <Category>Electronics</Category>
+        <Price>799.99</Price>
+        <Stock>15</Stock>
+        <Description>A powerful laptop for work and play.</Description>
+        <ImageURL>http://example.com/images/laptop.jpg</ImageURL>
+    </Product>
+</Catalog>`;
+
+module.exports = xmlData;
